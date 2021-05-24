e70b82726c488017475905398f693a9ae86e2ade
Nikolas Bompetsis <nbompetsis@di.uoa.gr>
2021-05-24 03:12:54 +0300

    Removed unused functions and minor refactoring

--- DBLPParser/src/parser.py
+++ DBLPParser/src/parser.py
@@ -1,16 +1,6 @@
 from lxml import etree
 from datetime import datetime
 import csv
-import codecs
-import ujson
-import re
-
-# all of the element types in dblp
-all_elements = {"article", "inproceedings", "proceedings", "book", "incollection", "phdthesis", "mastersthesis", "www"}
-# all of the feature types in dblp
-all_features = {"address", "author", "booktitle", "cdrom", "chapter", "cite", "crossref", "editor", "ee", "isbn",
-                "journal", "month", "note", "number", "pages", "publisher", "school", "series", "title", "url",
-                "volume", "year"}
 
 
 def log_msg(message):
@@ -28,6 +18,20 @@ def clear_element(element):
     element.clear()
     while element.getprevious() is not None:
         del element.getparent()[0]
+
+
+def getAttribute(attr, defaultValue = ''):
+    if attr and len(attr) > 0:
+        return attr[0]
+    return defaultValue
+
+
+def getConferenceName(type, attr, defaultValue = ''):
+    if type == 'article':
+        return getAttribute(attr['journal'])
+    elif type == 'incollection':
+        return getAttribute(attr['booktitle']) 
+    return defaultValue
 
 
 def count_pages(pages):
@@ -97,81 +101,7 @@ def extract_feature(elem, features, include_key=False):
     return attribs
 
 
-def parse_all(dblp_path, save_path, include_key=False):
-    log_msg("PROCESS: Start parsing...")
-    f = open(save_path, 'w', encoding='utf8')
-    for _, elem in context_iter(dblp_path):
-        if elem.tag in all_elements:
-            attrib_values = extract_feature(elem, all_features, include_key)
-            f.write(str(attrib_values) + '\n')
-        clear_element(elem)
-    f.close()
-    log_msg("FINISHED...")  # load the saved results line by line using json
-
-
-def parse_entity(dblp_path, save_path, type_name, features=None, save_to_csv=False, include_key=False):
-    """Parse specific elements according to the given type name and features"""
-    log_msg("PROCESS: Start parsing for {}...".format(str(type_name)))
-    assert features is not None, "features must be assigned before parsing the dblp dataset"
-    results = []
-    attrib_count, full_entity, part_entity = {}, 0, 0
-    for _, elem in context_iter(dblp_path):
-        if elem.tag in type_name:
-            attrib_values = extract_feature(elem, features, include_key)  # extract required features
-            results.append(attrib_values)  # add record to results array
-            for key, value in attrib_values.items():
-                attrib_count[key] = attrib_count.get(key, 0) + len(value)
-            cnt = sum([1 if len(x) > 0 else 0 for x in list(attrib_values.values())])
-            if cnt == len(features):
-                full_entity += 1
-            else:
-                part_entity += 1
-        elif elem.tag not in all_elements:
-            continue
-        clear_element(elem)
-    if save_to_csv:
-        f = open(save_path, 'w', newline='', encoding='utf8')
-        writer = csv.writer(f, delimiter=',')
-        writer.writerow(features)  # write title
-        for record in results:
-            # some features contain multiple values (e.g.: author), concatenate with `::`
-            row = ['::'.join(v) for v in list(record.values())]
-            writer.writerow(row)
-        f.close()
-    else:  # default save to json file
-        with codecs.open(save_path, mode='w', encoding='utf8', errors='ignore') as f:
-            ujson.dump(results, f)
-    return full_entity, part_entity, attrib_count
-
-def parse_article(dblp_path, save_path, save_to_csv=False, include_key=False):
-    type_name = ['article']
-    features = ['title', 'author', 'year', 'journal', 'pages']
-    info = parse_entity(dblp_path, save_path, type_name, features, save_to_csv=save_to_csv, include_key=include_key)
-    log_msg('Total articles found: {}, articles contain all features: {}, articles contain part of features: {}'
-            .format(info[0] + info[1], info[0], info[1]))
-    log_msg("Features information: {}".format(str(info[2])))
-
-
-def parse_inproceedings(dblp_path, save_path, save_to_csv=False, include_key=False):
-    type_name = ["inproceedings"]
-    features = ['title', 'author', 'year', 'pages', 'booktitle']
-    info = parse_entity(dblp_path, save_path, type_name, features, save_to_csv=save_to_csv, include_key=include_key)
-    log_msg('Total inproceedings found: {}, inproceedings contain all features: {}, inproceedings contain part of '
-            'features: {}'.format(info[0] + info[1], info[0], info[1]))
-    log_msg("Features information: {}".format(str(info[2])))
-
-
-def parse_incollection(dblp_path, save_path, save_to_csv=False, include_key=False):
-    type_name = ["incollection"]
-    features = ['title', 'editor', 'year', 'booktitle', 'publisher', 'pages']
-    # Other features are 'volume','isbn' and 'url'.
-    info = parse_entity(dblp_path, save_path, type_name, features, save_to_csv=save_to_csv, include_key=include_key)
-    log_msg('Total proceedings found: {}, proceedings contain all features: {}, proceedings contain part of '
-            'features: {}'.format(info[0] + info[1], info[0], info[1]))
-    log_msg("Features information: {}".format(str(info[2])))
-
-
-def parse_author_publication_relation(relationship_data, attrib_values):
+def parse_author_publication_relations(relationship_data, attrib_values):
     if len(attrib_values['author']) == 1:
         relationship_data.append([attrib_values['author'][0], attrib_values['title'][0], 'first'])
     elif len(attrib_values['author']) > 1:
@@ -217,7 +147,7 @@ def parse_publication(dblp_path, authors_file, publication_file, relationship_fi
                 authors_data.update(a for a in attrib_values['author'])
                 publications_data_line = attrib_values['title'][0] + '|'  + elem.tag + '|' + getAttribute(attrib_values['year']) + '|' + conference_type + '|' + getConferenceName(elem.tag, attrib_values) + '|' + getAttribute(attrib_values['pages'])
                 publications_data.append(publications_data_line.split('|'))
-                parse_author_publication_relation(relationship_data, attrib_values)
+                parse_author_publication_relations(relationship_data, attrib_values)
 
             publication_cnt = publication_cnt - 1
             if publication_cnt == 0:
@@ -241,17 +171,6 @@ def create_file_with_header(file_path, headers, data, notSet=False):
         writer.writerows([d] for d in data)
     f.close()
 
-def getAttribute(attr, defaultValue = ''):
-    if attr and len(attr) > 0:
-        return attr[0]
-    return defaultValue
-    
-def getConferenceName(type, attr, defaultValue = ''):
-    if type == 'article':
-        return getAttribute(attr['journal'])
-    elif type == 'incollection':
-        return getAttribute(attr['booktitle']) 
-    return defaultValue
 
 def main():
     dblp_path = 'dataset/dblp.xml'
